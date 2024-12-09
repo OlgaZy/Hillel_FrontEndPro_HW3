123e93734e23d4c5490de81f42224465a8ac85e8
Olga <olgazykova27@gmail.com>
2024-12-09 12:13:59 +0200

    Template Strings

---
+++ js.js
@@ -0,0 +1,6 @@
+let firstString = prompt('Enter your first string:');
+let secondString = prompt('Enter your second string:');
+let thirdString = prompt('Enter your third string:');
+
+let allStrings = `${secondString}, ${firstString}, ${thirdString}`;
+console.log(allStrings);
